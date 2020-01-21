a48f6f2448c2b14f7bfb9750e5fb529fc0f1b06b
jpegzilla <eris@jpegzilla.com>
2020-01-20 20:06:50 -0500

    reorganized templates

---
+++ templates/react/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./components/App";
+import "./components/styles/main.min.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
