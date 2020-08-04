55e76eb3db7638c1e7f02f2b80cea9f3efaf5338
jpegzilla <eris@jpegzilla.com>
2020-08-04 13:26:40 -0400

    removed unnecessary things from main sass file

--- templates/react/src/components/styles/main.sass.js
+++ templates/react/src/components/styles/main.sass.js
@@ -1,15 +1,4 @@
 module.exports = `// compile main
 
-@import 'defaults'
-@import 'header'
-@import 'list'
-
-main
-  min-height: 100vh
-  font-family: sans-serif
-  padding-left: 1rem
-
-  header
-    padding: 0.5rem 0 0 0
-    margin-bottom: 1rem
+@use 'components/defaults'
 `
